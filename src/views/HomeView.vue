f9eac7ef6cf8761a07c118f0e7c84534aa561112
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2024-05-06 12:14:30 -0400

    instalacion Bootstrap

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <div class="card" style="width: 18rem;">
+  
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
